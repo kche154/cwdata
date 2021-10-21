f64b0c47984470cd0fa7a5f9733eeb6dbf084522
Kaihao <kche154@student.monash.edu>
2021-10-21 18:06:29 +1100

    update

--- R/key_crop_yields.R
+++ R/key_crop_yields.R
@@ -8,8 +8,8 @@
 #'
 #' @format A data frame with 53940 rows and 10 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{Entity}{Country}
+#'   \item{tonnes_per_hectare}{yield}
 #'   ...
 #' }
 #'
